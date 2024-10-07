332e327a70c6e075d80867ea82836fdf5a1c72c6
Ramon Pozuelo Ruiz <pozueloruizramon@gmail.com>
2024-10-07 13:16:47 +0200

    Update saving dictionaries  script

--- save_boostlets_dict.py
+++ save_boostlets_dict.py
@@ -8,6 +8,7 @@ from boostlets_mod import Boostlet_syst
 # Hydra --multirun (-m)
 # Reads the file ./configs/boost_sys.yaml but run this script with BS.M=120, then 140 and finally 160
 # python save_boostlets_dict.py -m BS.M=120,140,160
+# python ./save_boostlets_dict.py -m BS.n_v_thetas=3,7,15  BS.n_h_thetas=3,7,15
 
 @hydra.main(version_base=None, config_path="./configs", config_name="boost_sys")
 def main(cfg: DictConfig):
@@ -24,18 +25,26 @@ def main(cfg: DictConfig):
         dx=dx, dt=1/fs, cs=cs,
         M=M, N=N, 
         n_v_scales=n_v_scales, n_h_scales=n_h_scales,
-        n_v_thetas=n_v_thetas, n_h_thetas=n_h_thetas
+        n_v_thetas=n_v_thetas, n_h_thetas=n_h_thetas,
+        base_v=base_v, base_h=base_h, 
     )
 
-    # Retrieve boostlet dictionary
-    Psi = BS.get_boostlet_dict()
-
-    # Check if saving parameters are provided
-    if "folder" in cfg.saving_param:
+    try:
         folder = cfg.saving_param.folder
-        BS.save_dict(folder=folder)
+    except AttributeError:
+        folder = './saved_dicts'  # Valor predeterminado si no existe 'folder'
+
+    try:
+        dict_type = cfg.saving_param.dict_type
+    except AttributeError:
+        dict_type = 1  # Valor predeterminado si no existe 'dict_type'
+
+    if dict_type==1:
+        print("Saving Boostlet dict type 1")
+        BS.save_dict_1(folder=folder)
     else:
-        BS.save_dict()
+        print("Saving Boostlet dict type 2")
+        BS.save_dict_2(folder=folder)
 
 if __name__ == "__main__":
     main()
