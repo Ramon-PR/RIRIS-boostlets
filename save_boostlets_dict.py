a43b5d4bb3cc9f5a5c7bc8f1e90049f90fb8cb0a
Ramon Pozuelo Ruiz <pozueloruizramon@gmail.com>
2024-10-12 02:22:36 +0200

    Updated saving script with configs

--- save_boostlets_dict.py
+++ save_boostlets_dict.py
@@ -5,17 +5,19 @@ from omegaconf import DictConfig
 from scipy.io import savemat
 from boostlets_mod import Boostlet_syst
 
+# Examples of run with config files in hydra
 # Hydra --multirun (-m)
-# Reads the file ./configs/boost_sys.yaml but run this script with BS.M=120, then 140 and finally 160
+# Reads the file ./configs/main_save_BSdict.yaml but run this script with BS.M=120, then 140 and finally 160
 # python save_boostlets_dict.py -m BS.M=120,140,160
 # python ./save_boostlets_dict.py -m BS.n_v_thetas=3,7,15  BS.n_h_thetas=3,7,15
+# python save_boostlets_dict.py  dicts@BS=BS_tan saving_param.folder=./saved_tan_dict
 
-@hydra.main(version_base=None, config_path="./configs", config_name="boost_sys")
+@hydra.main(version_base=None, config_path="./configs", config_name="main_save_BSdict")
 def main(cfg: DictConfig):
     # Extract parameters from the config
     BS_params = cfg.BS
     M, N = BS_params.M, BS_params.N
-    dx, fs, cs = BS_params.dx, BS_params.fs, BS_params.cs
+    dx, fs, cs = BS_params.sampling.dx, BS_params.sampling.fs, BS_params.sampling.cs
     n_v_scales, n_h_scales = BS_params.n_v_scales, BS_params.n_h_scales
     base_v, base_h = BS_params.base_v, BS_params.base_h
     n_v_thetas, n_h_thetas = BS_params.n_v_thetas, BS_params.n_h_thetas
@@ -35,7 +37,7 @@ def main(cfg: DictConfig):
         folder = './saved_dicts'  # Valor predeterminado si no existe 'folder'
 
     try:
-        dict_type = cfg.saving_param.dict_type
+        dict_type = BS_params.dict_type
     except AttributeError:
         dict_type = 1  # Valor predeterminado si no existe 'dict_type'
 
